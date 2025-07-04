1627d7c17157b0c7996e2309aa4a3c62fadb36b6
Douglas Boardman <46564691+douglasboardman@users.noreply.github.com>
2025-07-04 00:37:57 -0300

    commit

---
+++ arrays.java
@@ -0,0 +1,17 @@
+public class arrays {
+    public static void main(String args[]) {
+        // int[] numbers;
+        // numbers = new int[5];
+
+        int[] numbers2 = {1, 5, 97, 3, 8};
+
+        numbers2[2] = 108;
+
+        // int firstNumber = numbers2[2];
+        // int n = numbers2.length;
+
+        for(int num : numbers2) {
+            System.out.println(num);
+        }
+    }
+}
